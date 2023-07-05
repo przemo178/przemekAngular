8c0b997923878dcf02513c5909e9b28e4328d333
Przemek Lisowski <przemek_lisowski@o2.pl>
2023-07-05 21:05:07 +0200

    add carousel service

--- mical-angular/src/app/mical/components/header/header.component.ts
+++ mical-angular/src/app/mical/components/header/header.component.ts
@@ -2,25 +2,28 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { MenuService } from '../../services/menu.service';
 import { BehaviorSubject } from 'rxjs';
+import { CarouselService } from '../../services/carousel.service';
+import { ICarouselItem } from '../../models/menu.interface';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   @Output() public buttonClick: EventEmitter<boolean> =
     new EventEmitter<boolean>();
 
-    public isAuthorized$: BehaviorSubject<boolean> = this._menuService.isAuthorize$;
+  public isAuthorized$: BehaviorSubject<boolean> =
+    this._menuService.isAuthorize$;
 
-    constructor(private _menuService: MenuService) {}
+  constructor(private _menuService: MenuService) {}
 
   onClick(isOpen: boolean) {
     this.buttonClick.emit(isOpen);
   }
 
-  setAuthotrize() {
-    this._menuService.menuItems$.next([{name: 'Dupa'}])
-  }
+  // setAuthotrize() {
+  //   this._menuService.menuItems$.next([{name: 'Dupa'}])
+  // }
 }
